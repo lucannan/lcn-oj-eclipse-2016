5ca7341dae77a8e432ac886346f7fbd627f5cc4c
samuellcn <812481699@qq.com>
2016-10-02 18:26:07 +0800

    sort stack by stack

--- src/main/java/程序员代码面试指南/section01/sortStackByStack.java
+++ src/main/java/程序员代码面试指南/section01/sortStackByStack.java
@@ -1,5 +1,21 @@
 package 程序员代码面试指南.section01;
 
+import java.util.*;
+
 public class sortStackByStack {
 
+	public static void sortStackByStack(Stack<Integer> stack) {
+		Stack<Integer> help = new Stack<Integer>();
+		while (!stack.isEmpty()) {
+			int cur = stack.pop();
+			while ((!help.isEmpty()) && help.peek() < cur) {
+				stack.push(help.pop());
+			}
+			help.push(cur);
+		}
+
+		while (!help.isEmpty()) {
+			stack.push(help.pop());
+		}
+	}
 }
---
+++ src/main/java/程序员面试经典/secton09/anagram_9_1_3.java
@@ -0,0 +1,5 @@
+package 程序员面试经典.secton09;
+
+public class anagram_9_1_3 {
+
+}

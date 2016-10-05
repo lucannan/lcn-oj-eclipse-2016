900140ea0bca882e1fd96c85252121c24e0426cc
samuellcn <812481699@qq.com>
2016-10-05 19:13:27 +0800

    leetcode oj

---
+++ src/main/java/leetcode/Solution_225.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class Solution_225 {
+
+	// Push element x onto stack.
+	public void push(int x) {
+
+	}
+
+	// Removes the element on top of the stack.
+	public void pop() {
+
+	}
+
+	// Get the top element.
+	public int top() {
+
+		return 0;
+	}
+
+	// Return whether the stack is empty.
+	public boolean empty() {
+
+		return true;
+	}
+}
---
+++ src/main/java/leetcode/Solution_231.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class Solution_231 {
+	public boolean isPowerOfTwo(int n) {
+		return n > 0 && Integer.bitCount(n) == 1;
+	}
+}
